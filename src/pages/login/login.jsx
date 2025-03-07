e93e7ca0f065060fd2f921312da138cadf7d901c
LuaFly <luasoestudos@yahoo.com>
2025-03-07 10:02:56 -0300

    login e registro ok, automatizado ao fazer alteração, é alterado sem precisar desligar o servidor

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
-const login = () => {
-    return(
-        <div>
-            Entrar
-        </div>
-    )
-}
-export default login;
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="login-text-wrapper">
+        <h2 className="login-title">Entrar</h2>
+        <form className="login-form">
+          <input type="email" placeholder="Digite seu e-mail" className="login-input" />
+          <input type="password" placeholder="Digite sua senha" className="login-input" />
+          <button type="submit" className="login-button">Entrar</button>
+        </form>
+        <p className="login-text">
+          Não tem uma conta? <Link to="/register" className="login-register-link">Cadastre-se</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
