25dabb5267526d6c9c7dd474ecb566151d4f6f7b
LuaFly <luasoestudos@yahoo.com>
2025-04-22 10:00:44 -0300

    pendencias

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
-import Footer from "./components/footer/footer";  
 import React from "react";
 import './App.css';
 import Header from './components/header/header'; 
+import Footer from "./components/footer/footer";  
 import Home from './pages/home'; 
 import Inicio from './pages/pos_login/inicio'; 
 import Login from './pages/login/login';
@@ -10,26 +9,42 @@ import Cadastrar from './pages/cadastro/cadastrar';
 import Feed from "./pages/pos_login/feed";
 import Missao from './pages/missao';
 
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import PrivateRoute from './components/PrivateRoute'; // <== importa aqui
 
 function App() {
     return (
-      <div>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-          <Route path="/" element={<Home />} /> 
-          <Route path="/login" element={<Login />} /> 
-          <Route path="/inicio" element={<Inicio />} /> 
-          <Route path="/feed" element={<Feed />} /> 
-           <Route path="/cadastrar" element={<Cadastrar />} />
-           <Route path="/missao" element={<Missao />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </div>
+        <div>
+            <BrowserRouter>
+                <Header /> 
+                <Routes>
+                    <Route path="/" element={<Home />} /> 
+                    <Route path="/login" element={<Login />} /> 
+                    <Route path="/cadastrar" element={<Cadastrar />} />
+                    <Route path="/missao" element={<Missao />} />
+
+                    {/* Rotas protegidas */}
+                    <Route 
+                        path="/inicio" 
+                        element={
+                            <PrivateRoute>
+                                <Inicio />
+                            </PrivateRoute>
+                        } 
+                    />
+                    <Route 
+                        path="/feed" 
+                        element={
+                            <PrivateRoute>
+                                <Feed />
+                            </PrivateRoute>
+                        } 
+                    />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </div>
     );
-  }
-  
-  export default App;
-  
+}
+
+export default App;
